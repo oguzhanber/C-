ab32ad5ef39e5a451e503635832d450c66825c48
oguzhanber <32467282+oguzhanber@users.noreply.github.com>
2017-11-08 22:43:48 +0300

    Update Kalitim.cs

--- Kalitim.cs
+++ Kalitim.cs
@@ -12,7 +12,7 @@ class Program
 
             Ogrenci o1 = new Ogrenci("Mehmet Yilmaz");
             o1.Yas = 149;
-            o1.Ad = "Ömer";
+            o1.Ad = "Oğuz";
             o1.BilgiYaz();
             o1.Oku();
 
